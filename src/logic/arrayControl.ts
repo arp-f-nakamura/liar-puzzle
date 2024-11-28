4c866c6b504f77def25f227c0d8c13d542053a1f
arp-f-nakamura <f.nakamura.arp@gmail.com>
2024-11-28 23:43:04 +0900

    arrayControlロジック修正

--- src/logic/arrayControl.ts
+++ src/logic/arrayControl.ts
@@ -1,12 +1,22 @@
 import { liarFlg, nameFlg, portraits, specifyFlg } from "../constant/constant";
 import { Question } from "../type/type";
 
+/** 証言内容 */
+const commentContent = (flg: boolean, specify: number): string => {
+  if (flg === true) {
+    return `${nameFlg[specify]}は正直者だよ`;
+  } else if (flg === false) {
+    return `${nameFlg[specify]}は嘘つきだよ`;
+  }
+  return "error";
+};
+
 /** 嘘つきパズルを作るロジック */
 export const arrayControl = (): Question[] => {
   const shuffleArray = (array: any[]) => {
     for (let i = array.length - 1; i > 0; i--) {
-      // 0 <= j < iでランダムな整数を設定
-      const j = Math.floor(Math.random() * (i + 1));
+      // 0 <= j < 6でランダムな整数を設定
+      const j = Math.floor(Math.random() * 6);
       // i番目の要素の中身をとっておく
       const element = array[i];
       // i番目をj番目に移動
@@ -25,21 +35,12 @@ export const arrayControl = (): Question[] => {
     let comment = "";
     /** i番目の証言対象者 */
     const specify = specifyFlg[i];
-    /** 証言内容 */
-    const commentContent = (flg: boolean, specify: number): string => {
-      if (flg === true) {
-        return `${nameFlg[specify]}は正直者だよ`;
-      } else if (flg === false) {
-        return `${nameFlg[specify]}は嘘つきだよ`;
-      }
-      return "error";
-    };
 
     if (liarFlg[i] === 1) {
       /** i番目が嘘つきだった場合、嘘の発言をする */
       if (specify === i) {
         // 0<= j < 6
-        const j = Math.floor(Math.random() * (6 + 1));
+        const j = Math.floor(Math.random() * 6);
         if (j === i) {
           comment = "僕は正直者だよ";
         } else {
@@ -52,7 +53,7 @@ export const arrayControl = (): Question[] => {
       /** i番目がほんとつきだった場合、本当の発言をする */
       if (specify === i) {
         // 0<= j < 6
-        const j = Math.floor(Math.random() * (6 + 1));
+        const j = Math.floor(Math.random() * 6);
         if (j === i) {
           comment = "僕は正直者だよ";
         } else {
